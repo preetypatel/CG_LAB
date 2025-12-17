7f28c8af486fed9efc111ad67679f98085c74815
preetypatel <preetypatel930@gmail.com>
2025-12-17 13:09:36 +0545

    Create lab1.cpp

---
+++ Lab 1/lab1.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <graphics.h>
+int main()
+{
+    int gm, x, y, gd = DETECT, i;
+    // int midx, midy;
+    int stangle = 45, endangle = 50;
+    int radius = 50;
+    
+    char data[] = "C:\\MinGW\\lib\\libbgi.a"; //static file
+
+    initgraph(&gd, &gm, data);
+putpixel(100,100,WHITE);
+    getch();
+    closegraph();
+
+    return 0;
+}
